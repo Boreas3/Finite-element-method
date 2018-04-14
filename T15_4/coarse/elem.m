d3e2cc3dcbbaf90279da23a16054fd12e3a602d2
Bertrand-Schyns <37364787+Bertrand-Schyns@users.noreply.github.com>
2018-04-14 16:50:44 +0200

    Add files via upload

---
+++ T15_4/coarse/elem.m
@@ -0,0 +1,24 @@
+%10 éléments, Force = -15900000N
+ele5 = 1.469999E+04;
+nb5 = 44129;
+%8 éléments, Force = 
+ele6 = 1.56967104E+04;
+nb6 = 30773
+%8 éléments, Force = -16600000N
+ele7 = 1.58365252E+04;
+nb7 = 22877
+%5 élément, Force = -19000000
+ele10 = 2.096511E+04 ;
+nb10 = 11328;
+%4 éléments, Force = -20200000
+ele15 =  2.32516286E+04;
+nb15 = 5092;
+%3 élément , Force = -23200000
+ele20 = 3.06422355E+04;
+nb20 = 2923;
+%3 éléments , Force = -25000000
+ele25 = 3.472358E+04
+nb25 = 1878;
+x = [nb25 nb20 nb15 nb10 nb7 nb6 nb5]
+y = [ele25 ele20 ele15 ele10 ele7 ele6 ele5]
+plot(x,y)
