98561b8af63d0c035f5f85ef5b3244c51f542be4
Boreas3 <martindelongpre@hotmail.com>
2018-04-22 08:43:03 +0200

    Update simple_graphs.m

--- simple_graphs.m
+++ simple_graphs.m
@@ -4,7 +4,7 @@
 elem_15_4=[500 2892 11160 43473];
 elem_15_8=[500 2892 11160 43473];
 elem_26_3=[988 3621 5653 23572 94116];
-elem_26_6=988 3621 5653 23572 94116];
+elem_26_6=[988 3621 5653 23572 94116];
 % 2) Nombre de degrés de liberté
 ddl_15_4=[3074 17529 67309 262531];
 ddl_15_8=[3149 17707 67659 262225];
@@ -22,7 +22,7 @@ temps_15_4=[0.02 0.06 0.31 1.33];
 temps_15_8=[0.03 0.24 0.64 2.55];
 temps_26_3=[0.02 0.03 0.05 0.22 0.83];
 temps_26_6=[0.01 0.09 0.17 0.71 3.42];
-
+figure;
 %% TPE en fonction du nombre d'éléments
 plot(elem_15_4,tpe_15_4)
 hold on
@@ -32,9 +32,10 @@ plot(elem_26_3,tpe_26_3)
 hold on
 plot(elem_26_6,tpe_26_6)
 xlabel('Nombre d''éléments')
-ylabel('TPE (J)')
-title('TPE en fonction du nombre d''éléments')
+ylabel('|TPE| (J)')
+title('|TPE| en fonction du nombre d''éléments')
 legend('[T15](4)','[T15](8)','[T26](3)','[T26](6)','location','best'); 
+figure;
 %% TPE en fonction du nombre de ddl
 plot(ddl_15_4,tpe_15_4)
 hold on
@@ -44,9 +45,10 @@ plot(ddl_26_3,tpe_26_3)
 hold on
 plot(ddl_26_6,tpe_26_6)
 xlabel('Nombre de degrés de liberté')
-ylabel('TPE (J)')
-title('TPE en fonction du nombre de degrés de liberté')
+ylabel('|TPE| (J)')
+title('|TPE| en fonction du nombre de degrés de liberté')
 legend('[T15](4)','[T15](8)','[T26](3)','[T26](6)','location','best');
+figure;
 %% Temps de calcul en fonction du nombre d'éléments
 plot(elem_15_4,temps_15_4)
 hold on
@@ -59,6 +61,7 @@ xlabel('Nombre d''éléments')
 ylabel('Temps de calcul (s)')
 title('Temps de calcul en fonction du nombre d''éléments')
 legend('[T15](4)','[T15](8)','[T26](3)','[T26](6)','location','best');
+figure;
 %% Temps de calcul en fonction du nombre de ddl
 plot(ddl_15_4,temps_15_4)
 hold on
